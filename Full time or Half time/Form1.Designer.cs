b7c588824b1c9295c29d3e98c09cdf6d7a566dd0
Connor Johnson <io5774tr@go.minnstate.edu>
2020-05-03 22:25:30 -0500

    second commit

---
+++ Full time or Half time/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Full_time_or_Half_time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCredits = new System.Windows.Forms.TextBox();
+            this.btnWhatStatus = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of credits";
+            // 
+            // txtCredits
+            // 
+            this.txtCredits.Location = new System.Drawing.Point(327, 59);
+            this.txtCredits.Name = "txtCredits";
+            this.txtCredits.Size = new System.Drawing.Size(146, 31);
+            this.txtCredits.TabIndex = 1;
+            // 
+            // btnWhatStatus
+            // 
+            this.btnWhatStatus.Location = new System.Drawing.Point(71, 126);
+            this.btnWhatStatus.Name = "btnWhatStatus";
+            this.btnWhatStatus.Size = new System.Drawing.Size(402, 37);
+            this.btnWhatStatus.TabIndex = 2;
+            this.btnWhatStatus.Text = "What\'s my status?";
+            this.btnWhatStatus.UseVisualStyleBackColor = true;
+            this.btnWhatStatus.Click += new System.EventHandler(this.btnWhatStatus_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(121, 198);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(307, 53);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status shown here";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 297);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnWhatStatus);
+            this.Controls.Add(this.txtCredits);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCredits;
+        private System.Windows.Forms.Button btnWhatStatus;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
